233637d357a0be728eec253e8c62f300d3f3c3b9
maitrik4346 <maitrikkhunt@gmail.com>
2026-08-18 14:21:41 +0530

    Add files via upload

---
+++ Unit 2/U2_P2.java
@@ -0,0 +1,29 @@
+//Write a java program to use multi-level inheritance.
+
+class Animal{
+    void eat (){
+          System.out.println("This animal eats food.");
+    }
+  
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("The dog barks:Woof!Woof!");
+    }
+}
+
+class Puppy extends Dog{
+    void weep(){
+        System.out.println("Puppy is weeping.");
+    }
+}
+
+public class U2_P2{
+    public static void main(String args[]){
+        Puppy p = new Puppy();
+        p.eat();
+        p.bark();
+        p.weep();
+    }
+}

e2b8d1f6d84a3385f3264febd54d9953dc7d9c07
maitrik4346 <maitrikkhunt@gmail.com>
2026-08-18 14:19:56 +0530

    Add files via upload

---
+++ U2_P1.java
@@ -0,0 +1,22 @@
+//Write a java program to use simple inheritance.
+
+class Animal{
+    void eat (){
+          System.out.println("This animal eats food.");
+    }
+  
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("The dog barks:Woof!Woof!");
+    }
+}
+
+public class U2_P1{
+    public static void main(String args[]){
+        Dog mydog = new Dog();
+        mydog.eat();
+        mydog.bark();
+    }
+}
